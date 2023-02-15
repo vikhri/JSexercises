f0beca3609f78c8e77b0d657b7e9ad04fa2bdaa0
vikhri <104575543+vikhri@users.noreply.github.com>
2023-02-15 14:38:11 +0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,11 @@
 let numbers = (begin, end) => {
   for (let i = begin; i <= end; i++) {
-    if (i % 3 === 0 && i % 5 !== 0) {
-      console.log("делится на 3");
-    } else if (i % 5 === 0 && i % 3 !== 0) {
-      console.log("делится на 5");
-    } else if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log("делится на 3 и 5");
+    } else if (i % 5 === 0) {
+      console.log("делится на 5");
+    } else if (i % 3 === 0) {
+      console.log("делится на 3");
     } else {
       console.log(i);
     }
